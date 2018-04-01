bca479bfd4d33e8c65f8ca619074b9ff93a42d95
nishant <nishant.singh87@gmail.com>
2018-04-01 12:54:55 +0530

    Adding command runner.

--- src/commands/CommandRunner.js
+++ src/commands/CommandRunner.js
@@ -1,10 +1,19 @@
+const FindRelative = require('./FindRelative');
+
 class CommandRunner{
   constructor(relationships){
-
+    this.commands = [
+        new FindRelative(relationships)
+    ];
+    this.unknownCommandMessage =
+        'Available Commands : \n' +
+        this.commands.map(c => c.usage()).join('\n');
   }
 
-  interpret(command){
-    return 'result of ' + command;
+  interpret(input){
+    const command = this.commands.find(command => command.appliesTo(input));
+    return command ? command.execute(input) : this.unknownCommandMessage;
   }
 }
+
 module.exports = CommandRunner;
